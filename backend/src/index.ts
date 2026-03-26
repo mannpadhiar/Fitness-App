eff2abad6260c8554532a9f1495f04905ff55821
mannpadhiar <mannpadhiar22@gmail.com>
2026-03-26 14:29:13 +0530

    hosted backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,6 @@ import app from "./app.js";
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-  console.log(`📋 Health check: http://localhost:${PORT}/api/health`);
+  console.log(`🚀 Server running on ${process.env.BASE_URL}`);
+  console.log(`📋 Health check: ${process.env.BASE_URL}/api/health`);
 });
